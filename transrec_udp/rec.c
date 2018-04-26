65b4049aeee3f746fb37aec5f9f704bcdcaed43a
mzboito <mzboito@gmail.com>
2018-04-26 18:45:47 +0200

    cleaning

--- transrec_udp/rec.c
+++ transrec_udp/rec.c
@@ -76,17 +76,11 @@ int main(int argc, char **argv){
 	 }
 
 	 printf("Socket inicializado. Aguardando mensagens...\n\n");
-
-// Recebe pacotes do cliente e responde com string "ACK"
 	 while (1)
 	 {
 		 // Quando recebe um pacote, automaticamente atualiza o IP da estrutura peer
 		 rc = recvfrom(s,buffer,sizeof(buffer),0,(struct sockaddr *) &peer,(socklen_t *)&peerlen);
-		 //printf("Recebido %s\n", &buffer);
-		 //strcpy(buffer,"ACK");
 		 sendto(s,buffer,sizeof(buffer),0,(struct sockaddr *)&peer, peerlen);
-		 //printf("Enviado ACK\n\n");
-
 	 }
 
 }
